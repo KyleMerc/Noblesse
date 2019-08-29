f0d16d513f609552d698edff45be68cf74f0f1a9
Kyle Mercado <kyle.mercado@fullspeedtechnologies.com>
2019-08-29 17:18:05 +0800

    Added intro dialogue

--- main.php
+++ main.php
@@ -4,14 +4,23 @@ require_once "Noblesse/start.php";
 
 use Noblesse\Utility\MainUtil as Char;
 use Noblesse\Utility\Status;
+use Noblesse\Dialogue\IntroDialogue as Intro;
 
-$mainChar = Char::mainCharacter('h');
-$enemy = Char::enemyCharacter('v');
+$opt = Intro::mainMenu();
 
+$mainChar = Char::mainCharacter($opt);
+$name = $mainChar->getName();
+// $enemy = Char::enemyCharacter('v');
+
+echo "\t" . $name . " has been chosen\n";
+
+$dialogue = Intro::introDialogue($name);
+echo $dialogue['intro'];
+echo $dialogue['charIntro'];
 // echo $mainChar->getModHumanType();
 // echo Status::status($enemy) . "\n";
-echo $enemy->attack($mainChar) . "\n";
-echo $enemy->attack($mainChar) . "\n";
-echo $enemy->attack($mainChar) . "\n";
+// echo $enemy->attack($mainChar) . "\n";
+// echo $enemy->attack($mainChar) . "\n";
+// echo $enemy->attack($mainChar) . "\n";
 
-echo Status::status($mainChar) . "\n";
+// echo Status::status($mainChar) . "\n";
